87e7a6ea20f103a029f757e95ca1a88c47a1b98b
Saravanan <Saravanan@DESKTOP-97U9QHO>
2024-06-24 06:39:27 +0530

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPAPI.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+    }
+}
